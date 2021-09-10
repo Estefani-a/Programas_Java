b9f69532076dfc465168a4f9ebff4fbcea05d61c
Maria Estefanía Sassone <estefaniasassone@gmail.com>
2021-09-10 03:51:16 -0300

    Update Promedio.java

--- Practica 1/Promedio.java
+++ Practica 1/Promedio.java
@@ -1,13 +1,13 @@
 /*Realizar un programa que permita a un profesor calcular el promedio de un alumno. Para esto, el algoritmo debe 
 permitir ingresar las 4 notas del mismo (por ejemplo, 8, 7, 9.50 y 10), luego calcular el promedio de las mismas y 
 mostrar el resultado por pantalla. Se recuerda que el promedio es la suma de todas las notas dividido la cantidad, en 
-el ejemplo sería: (8+7+9.50+10)/4.*/
+el ejemplo serÃ­a: (8+7+9.50+10)/4.*/
 
 package practica1;
 
 import java.util.Scanner;
 
-public class E1P1{
+public class Promedio{
 
     public static void main(String[] args){
         float nota1, nota2, nota3, nota4, promedio;
